28c0889674af1f4b24e26e01c7736c31eed3d2d3
akashkumar-1808 <akashkumargunturu18@gmail.com>
2026-07-24 23:04:02 +0530

    Name package

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ module.exports = {
       bundleIdentifier: "org.nirvaha.app"
     },
     android: {
-      package: "com.nirvaha.app",
+      package: "org.nirvaha.app",
       googleServicesFile: "./google-services.json",
       versionCode: 3,
 
@@ -44,53 +44,53 @@ module.exports = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     web: {
-    output: "static",
-    favicon: "./assets/images/favicon.png"
-  },
-  plugins: [
-    withNotificationColorFix,
-    "expo-router",
-    [
-      "expo-splash-screen",
-      {
-        "image": "./assets/images/icon.png",
-        "imageWidth": 200,
-        "resizeMode": "contain",
-        "backgroundColor": "#082F03",
-        "dark": {
-          "backgroundColor": "#082F03"
-        }
-      }
-    ],
-    "expo-font",
-    "expo-audio",
-    "expo-video",
-    "@react-native-firebase/app",
-    [
-      "expo-notifications",
-      {
-        "icon": "./assets/notification-icon.png",
-        "color": "#2d5a4c"
-      }
-    ]
-  ],
-  experiments: {
-    typedRoutes: true,
-    reactCompiler: true
-  },
-  extra: {
-    eas: {
-      projectId: "fee74e4b-51d9-4c95-a160-c11d63160417"
+      output: "static",
+      favicon: "./assets/images/favicon.png"
     },
-    "EXPO_PUBLIC_API_BASE_URL": process.env.EXPO_PUBLIC_API_BASE_URL || "",
-    "EXPO_PUBLIC_FIREBASE_API_KEY": process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "",
-    "EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN": process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-    "EXPO_PUBLIC_FIREBASE_PROJECT_ID": process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "",
-    "EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET": process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-    "EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID": process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-    "EXPO_PUBLIC_FIREBASE_APP_ID": process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "",
-    "EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID": process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
-  }
+    plugins: [
+      withNotificationColorFix,
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#082F03",
+          "dark": {
+            "backgroundColor": "#082F03"
+          }
+        }
+      ],
+      "expo-font",
+      "expo-audio",
+      "expo-video",
+      "@react-native-firebase/app",
+      [
+        "expo-notifications",
+        {
+          "icon": "./assets/notification-icon.png",
+          "color": "#2d5a4c"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      eas: {
+        projectId: "fee74e4b-51d9-4c95-a160-c11d63160417"
+      },
+      "EXPO_PUBLIC_API_BASE_URL": process.env.EXPO_PUBLIC_API_BASE_URL || "",
+      "EXPO_PUBLIC_FIREBASE_API_KEY": process.env.EXPO_PUBLIC_FIREBASE_API_KEY || "",
+      "EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN": process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+      "EXPO_PUBLIC_FIREBASE_PROJECT_ID": process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || "",
+      "EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET": process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+      "EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID": process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+      "EXPO_PUBLIC_FIREBASE_APP_ID": process.env.EXPO_PUBLIC_FIREBASE_APP_ID || "",
+      "EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID": process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
+    }
   }
 }
 
